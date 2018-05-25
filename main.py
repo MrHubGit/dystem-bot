8b3f96ce7c800ca751fad96d3dd5cabb36c1bab5
AnonRobot <james.woodbridge@gmail.com>
2018-05-25 11:39:40 +0100

    Added more cats and help command

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.voice_client import VoiceClient
 import asyncio 
 import time 
+from random import randint
 
 Client = discord.Client() 
 command_bot = commands.Bot(command_prefix="!", description="Hi! I am the dystem bot. Beep.")
@@ -16,7 +17,7 @@ async def on_ready():
 @command_bot.command(pass_context=True)
 async def test(ctx):
     embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
-    embed.add_field(name="Author", value="<YOUR-USERNAME>")
+    embed.add_field(name="Author", value="Dystem")
     embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
     await ctx.send(embed=embed)
 
@@ -29,7 +30,35 @@ async def on_message(message):
         await command_bot.delete_message(message)
     
     if (message.content == "!cat"):
-        await command_bot.send_message(message.channel, "<@{}> has triggered the cat https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif".format(message.author.id) )
+        randomCat = randint(0, 9) + 1
+        cat_switcher = {
+            1: "http://i0.kym-cdn.com/photos/images/newsfeed/000/431/312/67e.jpg",
+            2: "https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif",
+            3: "https://media.mnn.com/assets/images/2013/05/ultimatebrutality.jpg",
+            4: "https://i.amz.mshcdn.com/r7JSirBOG3KMCExw_iWEiLrueVs=/1200x627/2012%2F12%2F04%2F9d%2F15bestcatme.aH8.jpg",
+            5: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWqAPeRZlwct4xiMuAAvoYIK2tsmS3HgKI5yFpdqVAr79fC6W9cA",
+            6: "https://i.pinimg.com/originals/1d/c3/51/1dc3519cc3ec8d1148068fd08f42dcd3.jpg",
+            7: "https://i.imgur.com/nbNeUCC.gif?noredirect",
+            8: "http://2.bp.blogspot.com/-ESreZHheo1M/TxBchPbryFI/AAAAAAAAPn4/gVXXvRth6z4/s1600/monorail.jpeg",
+            9: "https://www.sklarwilton.com/wp-content/uploads/2011/10/i-can-has-cheezburger.jpg"
+        }
+        await command_bot.send_message(message.channel, "{}".format(cat_switcher.get(randomCat, "https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif")) )
+
+    if message.content.startswith("!help"):
+        output = "Public blockchainot commands:\n```css\n"
+        output = output + "!about : displays current blockchain and prices stats. \n"
+        output = output + "!invitelist : lists the top 20 inviters for the server. \n"
+        output = output + "!cat : displays a random cat meme for danystem. \n"
+        output = output + "!help : lists all of the dystem bot commands and their usage. \n"
+        output = output + "```"
+        await command_bot.send_message(message.channel, "{}".format(output))
+    
+    if message.content.startswith("!about"):
+        output = "<:dystem:445979324210741250> Dystem - Empowering open source development.\n```"
+        output = output + "Price: TBC \n"
+        output = output + "Block height: TBC \n"
+        output = output + "```"
+        await command_bot.send_message(message.channel, "{}".format(output))
 
     if message.content.startswith("!invitelist"):
         server = message.channel.server
